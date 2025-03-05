0410a0c82f264368d6bba9eacef9573b55a063d0
Aindrila Mukherjee <64476010+aindm98@users.noreply.github.com>
2025-03-06 00:26:15 +0530

    save changes in the contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,12 +2,12 @@ import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 
 const Contact = () => {
-    return (
-        <Container fluid className="about-section">
-        <Particle />
-        <Container>
-            <Row  style={{ justifyContent: "start", padding: "10px" }}>
-            <Col
+  return (
+    <Container fluid className="about-section">
+      <Particle />
+      <Container>
+        <Row style={{ justifyContent: "start", padding: "10px" }}>
+          <Col
             md={7}
             style={{
               justifyContent: "center",
@@ -16,30 +16,44 @@ const Contact = () => {
             }}
           >
             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-            Get In <strong className="purple"> Touch.</strong>
+              Get In <strong className="purple"> Touch.</strong>
             </h1>
-            <p className="">Have a question or want to work together?</p>
-           
+            <p className="mb-0" >Thanks for taking the time to reach out.</p>
+            <p className="mb-0">Have a question or want to work together?</p>
           </Col>
-          <Col sm={12} md={12} lg={6}></Col>
           <Col sm={12} md={12} lg={6}>
-          <form className="contact-form">
+          </Col>
+          <Col sm={12} md={12} lg={6}>
+            {/* <form className="contact-form">
           <div className="form-group ">
                 <input id="form_name1" className="form-control" type="text"   required />
                 <label htmlFor="form_name1">Name<span className="gl-form-asterisk"></span></label>
               </div>
               
-              <div className="form-group">
+              <div className="form-group mt-5">
                 <input id="form_name2" className="form-control" type="text"  placeholder="" required />
                 <label htmlFor="form_name2">Email<span className="gl-form-asterisk"></span></label>
               </div>  
-          </form>
+          </form> */}
+            <form className="contact-form">
+              <div className="input-group">
+                <input type="text" required />
+                <label htmlFor="name">Name</label>
+              </div>
+              <div className="input-group">
+                <input type="email" required />
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="input-group">
+              <textarea type="text" required rows="6"></textarea>
+              <label htmlFor="message" className="message_label" >Message</label>
+              </div>
+            </form>
           </Col>
-            </Row>
-        </Container>
-        </Container>
-    )
+        </Row>
+      </Container>
+    </Container>
+  );
 };
-
 
 export default Contact;
